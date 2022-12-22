2b31d3896ab66f53f6abb70e9f1169c10ba32d4a
gkfkktrh153 <77851079+gkfkktrh153@users.noreply.github.com>
2022-12-22 22:28:09 +0900

    jackson test

--- src/main/java/com/sy1/controller/PostController.java
+++ src/main/java/com/sy1/controller/PostController.java
@@ -80,10 +80,10 @@ public class PostController {
             }
         }
 
-        Gson gson = new Gson();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        String s = objectMapper.writeValueAsString(jsonPost);
 
-        String s = gson.toJson(jsonPost);
-        System.out.println("s :" + s);
         return s;
 
     }
