a390b4e7190a3f4041105e8653a776c590229eeb
gkfkktrh153 <gkfktkrh153@naver.com>
2023-01-08 01:32:07 +0900

    name API추가

--- src/main/java/com/sy1/controller/MemberController.java
+++ src/main/java/com/sy1/controller/MemberController.java
@@ -3,9 +3,13 @@ package com.sy1.controller;
 import com.sy1.dto.TokenInfo;
 import com.sy1.dto.MemberDTO;
 import com.sy1.entity.Member;
+import com.sy1.repository.MemberRepository;
 import com.sy1.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.json.simple.JSONObject;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
     private final MemberService memberService;
     private final ModelMapper  modelMapper;
+    private final MemberRepository memberRepository;
 
     @PostMapping("user/signup")
     public String signup(@RequestBody MemberDTO memberDTO)
@@ -28,8 +33,20 @@ public class MemberController {
         String memberId = memberDto.getEmail();
         String password = memberDto.getPassword();
         TokenInfo tokenInfo = memberService.login(memberId, password);
+
+
+
+
         return tokenInfo;
     }
+    @GetMapping("user/name")
+    public String getUserName(){
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        Member member = memberRepository.findByEmail(name).orElse(null);
+        return member.getName();
+    }
+
     @PostMapping("/user/test")
     public String test(){
         return "success";
